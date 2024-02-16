e0e86b5a9447881f83361b8b63ec351138511c02
Rune Soerensen <rsoerensen@salesforce.com>
2024-02-15 19:40:58 -0500

    Scaffold the movie model

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GettingStarted.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<GettingStartedMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("GettingStartedMovieContext") ?? throw new InvalidOperationException("Connection string 'GettingStartedMovieContext' not found.")));
 
 var app = builder.Build();
 
